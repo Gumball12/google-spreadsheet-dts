30c875a748cf7d3f1f37e5d8c0f8f312947fecfc
hj <to@shj.rip>
2024-02-25 17:58:29 +0900

    chore: setup build process

---
+++ src/parser/index.ts
@@ -0,0 +1,1 @@
+export { publicGoogleSheetsParser } from './publicGoogleSheetsParser';
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    lib: {
+      entry: {
+        index: resolve(__dirname, 'src/index.ts'),
+        parser: resolve(__dirname, 'src/parser/index.ts'),
+      },
+      name: 'googleSpreadsheetDts',
+    },
+  },
+});
